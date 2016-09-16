60cb9c213bd676d39595a9377615fba44fa38dd1
cmiller36 <m.calliemiller@gmail.com>
2016-09-16 17:41:00 -0400

    update my scrape method to extract word of the day

--- lib/wotd/word.rb
+++ lib/wotd/word.rb
@@ -11,12 +11,13 @@ class Wotd::Word
     # go to site, find wotd
     # extract properties
     # instantiate word
+    doc = Nokogiri::HTML(open("http://www.merriam-webster.com/word-of-the-day"))
     word = self.new
-    word.wotd = "wotd"
+    word.wotd = doc.search("h1").last.text
     word.definition = "definition"
     word.example = "example"
     word.fact = "fact"
     word
   end
-  
+
 end
